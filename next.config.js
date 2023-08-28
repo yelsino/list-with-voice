f4222a61e17c5554f1ed7398272ba1069975604a
yelsiño <yelsino321@gmai.com>
2023-08-28 17:46:51 -0500

    regresando a fetch

--- next.config.js
+++ next.config.js
@@ -18,7 +18,21 @@ const nextConfig = withPWA({
     env: {
         GPT_TOKEN: process.env.GPT_TOKEN
     },
-    // distDir: 'dist',
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.module.rules.push({
+                test: /\.(html)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next',
+                        name: 'static/media/[name].[hash].[ext]',
+                    },
+                },
+            });
+        }
+        return config;
+    },
 })
 
 module.exports = nextConfig
--- src/app/api/listas/[listaId]/print/route.ts
+++ src/app/api/listas/[listaId]/print/route.ts
@@ -12,91 +12,85 @@ interface IParams {
     };
 }
 
-// export async function GET(request: Request, { params }: IParams) {
-
-//     try {
-//         const { listaId } = params;
-    
-//         const listaBD = await obtenerLista(listaId ?? "")
-//         console.log('1');
-//         if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
-    
-//         // console.log(JSON.stringify(listaBD));
-        
-//         const templateHtml = await fetch('https://res.cloudinary.com/dwkfj5sxb/raw/upload/v1692990324/Templates/zv5f373tuh8j5ro5zga5.html')
-
-//         console.log('2222');
-//         let convertTemplate = await templateHtml.text();
-        
-//         console.log('33333');
-//         const imageBuffer = await nodeHtmlToImage({
-//             html: convertTemplate,
-//             output: undefined, // Do not write the image to a file
-//             type: "jpeg",
-//             content: listaToPrint(listaBD as Lista),
-//         });
-
-//         console.log('44444');
-        
-    
-//         const readableStream: any = new Readable();
-//         readableStream.push(imageBuffer);
-//         readableStream.push(null);
-//         console.log('5555');
-//         const response = new NextResponse(readableStream);
-//         console.log("response: ", response);
-//         console.log('6666');
-//         // response.headers.set("Content-Type", "image/jpeg");
-    
-//         return response;
-//     } catch (error) {
-//         console.log("ERROR:: ", error);
-        
-//     }
-// }
-
-
 export async function GET(request: Request, { params }: IParams) {
 
-    const { listaId } = params;
+    try {
+        const { listaId } = params;
     
-    const listaBD = await obtenerLista(listaId ?? "")
+        const listaBD = await obtenerLista(listaId ?? "")
+        console.log('1');
+        if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
     
-    if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
-
-    // console.log(JSON.stringify(listaBD));
-
-    
-    const filePath = path.resolve(__dirname, '..', '..', '..','..', '..','..','..','public', 'receipt.html');
-    console.log("filePath",filePath);
-
-    fs.readdir(filePath, (err,files)=>{
-        if(err) {
-            console.log('error al leer');
-            
-        }
-
-        console.log(files);
+        // console.log(JSON.stringify(listaBD));
         
-    })
+        const templateHtml = await fetch('https://res.cloudinary.com/dwkfj5sxb/raw/upload/v1692990324/Templates/zv5f373tuh8j5ro5zga5.html')
+
+        let convertTemplate = await templateHtml.text();
+        
+        const imageBuffer = await nodeHtmlToImage({
+            html: convertTemplate,
+            output: undefined, 
+            type: "jpeg",
+            content: listaToPrint(listaBD as Lista),
+        });
+        console.log('44444');
+        
+        const readableStream: any = new Readable();
+        readableStream.push(imageBuffer);
+        readableStream.push(null);
+
+        const response = new NextResponse(readableStream);
+        console.log("response: ", response);
+        // response.headers.set("Content-Type", "image/jpeg");
     
-    // console.log('ruta completa:', rutaCompleta);
-    
-    const content = await fs.promises.readFile(filePath, "utf8");
-
-    const imageBuffer = await nodeHtmlToImage({
-        html: content,
-        output: undefined, // Do not write the image to a file
-        type: "jpeg",
-        content: listaToPrint(listaBD as Lista),
-    });
-
-    const readableStream: any = new Readable();
-    readableStream.push(imageBuffer);
-    readableStream.push(null);
-
-    const response = new NextResponse(readableStream);
-    response.headers.set("Content-Type", "image/jpeg");
-
-    return response;
+        return response;
+    } catch (error) {
+        console.log("ERROR:: ", error);
+    }
 }
+
+
+// export async function GET(request: Request, { params }: IParams) {
+
+//     const { listaId } = params;
+    
+//     const listaBD = await obtenerLista(listaId ?? "")
+    
+//     if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
+
+//     // console.log(JSON.stringify(listaBD));
+
+    
+//     const filePath = path.resolve(__dirname, '..', '..', '..','..', '..','..','..','public', 'receipt.html');
+//     console.log("filePath",filePath);
+
+//     fs.readdir(filePath, (err,files)=>{
+//         if(err) {
+//             console.log('error al leer');
+            
+//         }
+
+//         console.log(files);
+        
+//     })
+    
+//     // console.log('ruta completa:', rutaCompleta);
+    
+//     const content = await fs.promises.readFile(filePath, "utf8");
+
+//     const imageBuffer = await nodeHtmlToImage({
+//         html: content,
+//         output: undefined, // Do not write the image to a file
+//         type: "jpeg",
+//         content: listaToPrint(listaBD as Lista),
+//     });
+
+//     const readableStream: any = new Readable();
+//     readableStream.push(imageBuffer);
+//     readableStream.push(null);
+
+//     const response = new NextResponse(readableStream);
+//     response.headers.set("Content-Type", "image/jpeg");
+
+//     return response;
+// }
