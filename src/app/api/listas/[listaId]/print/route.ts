106fd339d6973fd073f8caed6f4ed8d36246b4c4
yelsiño <yelsino321@gmai.com>
2023-08-28 14:09:48 -0500

    updte2

--- src/app/api/listas/[listaId]/print/route.ts
+++ src/app/api/listas/[listaId]/print/route.ts
@@ -1,13 +1,9 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-import * as fs from "node:fs";
-import path from "path";
 import nodeHtmlToImage from "node-html-to-image";
 import { Readable } from "stream";
 import { listaToPrint, obtenerLista } from "../../mapper/listas";
 import { Lista } from "@/interfaces/list.interface";
 
-const prisma = new PrismaClient();
 
 interface IParams {
     params: {
@@ -21,31 +17,35 @@ export async function GET(request: Request, { params }: IParams) {
         const { listaId } = params;
     
         const listaBD = await obtenerLista(listaId ?? "")
-        
+        console.log('1');
         if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
     
         // console.log(JSON.stringify(listaBD));
         
         const templateHtml = await fetch('https://res.cloudinary.com/dwkfj5sxb/raw/upload/v1692990324/Templates/zv5f373tuh8j5ro5zga5.html')
 
+        console.log('2222');
         let convertTemplate = await templateHtml.text();
-       
         
-        // const content = await fs.promises.readFile("public/receipt.html", "utf8")
-        
+        console.log('33333');
         const imageBuffer = await nodeHtmlToImage({
             html: convertTemplate,
             output: undefined, // Do not write the image to a file
             type: "jpeg",
             content: listaToPrint(listaBD as Lista),
         });
+
+        console.log('44444');
+        
     
         const readableStream: any = new Readable();
         readableStream.push(imageBuffer);
         readableStream.push(null);
-    
+        console.log('5555');
         const response = new NextResponse(readableStream);
-        response.headers.set("Content-Type", "image/jpeg");
+        console.log("response: ", response);
+        console.log('6666');
+        // response.headers.set("Content-Type", "image/jpeg");
     
         return response;
     } catch (error) {
