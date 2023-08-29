f7d9be2279c63f0d56c75fd3c5b9f7c40f07f32e
yelsiño <yelsino321@gmai.com>
2023-08-29 00:57:52 -0500

    cambios 006

--- next.config.js
+++ next.config.js
@@ -14,24 +14,25 @@ const withPWA = require('next-pwa')({
     dest: 'public'
 })
 
+const securityHeaders = [
+    // ...otros encabezados...
+    {
+      key: 'Content-Security-Policy',
+      value: '...',
+    },
+  ];
+
 const nextConfig = withPWA({
     env: {
         GPT_TOKEN: process.env.GPT_TOKEN
     },
-    webpack(config, { isServer }) {
-        if (!isServer) {
-            config.module.rules.push({
-                test: /\.(html)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        publicPath: '/_next',
-                        name: 'static/media/[name].[hash].[ext]',
-                    },
-                },
-            });
-        }
-        return config;
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
     },
 })
 
