01592f047eadfb3f49568d3bd7764fbb2f17164a
yelsiño <yelsino321@gmai.com>
2023-08-31 00:08:19 -0500

    canva p 3

--- src/app/api/listas/[listaId]/print/route.ts
+++ src/app/api/listas/[listaId]/print/route.ts
@@ -12,10 +12,6 @@ export async function GET(request: Request, { params }: IParams) {
     try {
         const { listaId } = params;
 
-        const data = {
-            // ... (tus datos aquí)
-        };
-
         const width = 800;
         const height = 600;
 
@@ -43,25 +39,6 @@ export async function GET(request: Request, { params }: IParams) {
         ctx.font = "bold 20px Arial";
         ctx.fillText(`Total: 500 soles`, 50, y + 20);
 
-        // Guardar la imagen generada en un archivo
-        // const out = fs.createWriteStream("lista-de-compras.png");
-        // const stream = canvas.createPNGStream();
-        // stream.pipe(out);
-        // out.on("finish", () => console.log("Imagen generada."));
-
-        // Convertir el canvas a un buffer (blob-like)
-        //  const buffer = canvasToBuffer(canvas, "image/png");
-
-        //  // Responder con el buffer al frontend
-        //  const response = new NextResponse(buffer, {
-        //      headers: {
-        //          "Content-Type": "image/png",
-        //      },
-        //  });
-
-        //  return response;
-
-
         const buffer = await new Promise<Buffer>((resolve, reject) => {
             canvas.toBuffer((err, buf) => {
                 if (err) {
@@ -88,46 +65,3 @@ export async function GET(request: Request, { params }: IParams) {
     }
 }
 
-// export async function GET(request: Request, { params }: IParams) {
-
-//     const { listaId } = params;
-
-//     const listaBD = await obtenerLista(listaId ?? "")
-
-//     if(!listaBD) return NextResponse.json({error:'No se encontró la lista'})
-
-//     // console.log(JSON.stringify(listaBD));
-
-//     const filePath = path.resolve(__dirname, '..', '..', '..','..', '..','..','..','public', 'receipt.html');
-//     console.log("filePath",filePath);
-
-//     fs.readdir(filePath, (err,files)=>{
-//         if(err) {
-//             console.log('error al leer');
-
-//         }
-
-//         console.log(files);
-
-//     })
-
-//     // console.log('ruta completa:', rutaCompleta);
-
-//     const content = await fs.promises.readFile(filePath, "utf8");
-
-//     const imageBuffer = await nodeHtmlToImage({
-//         html: content,
-//         output: undefined, // Do not write the image to a file
-//         type: "jpeg",
-//         content: listaToPrint(listaBD as Lista),
-//     });
-
-//     const readableStream: any = new Readable();
-//     readableStream.push(imageBuffer);
-//     readableStream.push(null);
-
-//     const response = new NextResponse(readableStream);
-//     response.headers.set("Content-Type", "image/jpeg");
-
-//     return response;
-// }
