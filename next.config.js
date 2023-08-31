827540315c2f3532c91fe7c93ac671cdb850f0a7
yelsiño <yelsino321@gmai.com>
2023-08-31 00:21:09 -0500

    canva p 7

--- next.config.js
+++ next.config.js
@@ -10,6 +10,17 @@
 
 /** @type {import('next').NextConfig} */
 
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+  ) {
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+  }
+
 const withPWA = require('next-pwa')({
     dest: 'public'
 })
