265a64b98eb80c338706d88d105bc55f5727f162
yelsiño <yelsino321@gmai.com>
2023-08-30 23:39:34 -0500

    canva prueba

--- src/app/api/listas/[listaId]/print/route.ts
+++ src/app/api/listas/[listaId]/print/route.ts
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
-import nodeHtmlToImage from "node-html-to-image";
 import { Readable } from "stream";
-import { listaToPrint, obtenerLista } from "../../mapper/listas";
-import { Lista } from "@/interfaces/list.interface";
-import puppeteer from "puppeteer";
-import * as phantom from "phantom";
+import * as fs from "fs";
+import { createCanvas, loadImage } from "canvas";
 
 interface IParams {
     params: {
@@ -15,19 +12,74 @@ export async function GET(request: Request, { params }: IParams) {
     try {
         const { listaId } = params;
 
-        const listaBD = await obtenerLista(listaId ?? "");
-        if (!listaBD)
-            return NextResponse.json({ error: "No se encontró la lista" });
+        const data = {
+            // ... (tus datos aquí)
+        };
 
-        // console.log(JSON.stringify(listaBD));
-        const instance: any = await phantom.create();
-        const page: any = await instance.createPage();
-        const status: string = await page.open("https://phantomjs.org/screen-capture.html");
-        const content = await page.property('content');
-        console.log(content);
-        await instance.exit();
+        const width = 800;
+        const height = 600;
 
-        return NextResponse.json({});
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+
+        // Dibujar un fondo
+        ctx.fillStyle = "white"; // Color de fondo
+        ctx.fillRect(0, 0, width, height);
+
+        // Dibujar el título
+        ctx.font = "bold 24px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText("Lista de Compras", 50, 50);
+
+        // Dibujar los detalles
+        let y = 100;
+        [1, 2, 3].forEach((item) => {
+            ctx.font = "18px Arial";
+            ctx.fillText(`items de lista numero 1`, 50, y);
+            y += 30; // Aumentar la posición vertical
+        });
+
+        // Dibujar el total
+        ctx.font = "bold 20px Arial";
+        ctx.fillText(`Total: 500 soles`, 50, y + 20);
+
+        // Guardar la imagen generada en un archivo
+        // const out = fs.createWriteStream("lista-de-compras.png");
+        // const stream = canvas.createPNGStream();
+        // stream.pipe(out);
+        // out.on("finish", () => console.log("Imagen generada."));
+
+        // Convertir el canvas a un buffer (blob-like)
+        //  const buffer = canvasToBuffer(canvas, "image/png");
+
+        //  // Responder con el buffer al frontend
+        //  const response = new NextResponse(buffer, {
+        //      headers: {
+        //          "Content-Type": "image/png",
+        //      },
+        //  });
+
+        //  return response;
+
+
+        const buffer = await new Promise<Buffer>((resolve, reject) => {
+            canvas.toBuffer((err, buf) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(buf);
+                }
+            });
+        });
+
+        const response = new NextResponse(buffer, {
+            headers: {
+                "Content-Type": "image/png",
+            },
+        });
+
+        return response;
+       
     } catch (error) {
         console.log("ERROR::  ", error);
         if (error) {
