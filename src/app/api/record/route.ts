a7c5a2caff7fa5f23464e2b9fe2d655adecafd65
yelsino <76143960+yelsino@users.noreply.github.com>
2023-10-11 07:35:09 +0000

    cambios basicos

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -14,10 +14,45 @@ export async function POST(request: Request) {
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
-  const filePath = path.join(process.cwd(),"public", "new.wav");
-  console.log("filePath: ", filePath);
-  
+  const filePath = path.join(process.cwd(), "public", "new.wav");
   await writeFile(filePath, buffer);
+
+
+  // CONVERTIR A VOZ
+  const deepgramApiKey = "754c19bbce9d48c2839a43f436ea954a804043fd";
+
+  const deepgram = new Deepgram(deepgramApiKey);
+  let source: any = null;
+
+  source = {
+    buffer: buffer,
+    mimetype: "http://127.0.0.1:3000",
+  };
+
+  // Send the audio to Deepgram and get the response
+  console.log('empezando....');
+  // console.time('transcripcion');
+  deepgram.transcription
+    .preRecorded(source, {
+      smart_format: true,
+      language: "es-419",
+      model: "nova",
+      // diarize: true,
+      multichannel: true,
+    detect_entities:true
+      // filler_words: true
+    })
+    .then((response: any) => {
+      console.dir(response, { depth: null });
+      console.log('respuesta....');
+    
+      // console.dir(response.results.channels[0].alternatives[0].transcript, { depth: null });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+
 
   return NextResponse.json({});
 }
