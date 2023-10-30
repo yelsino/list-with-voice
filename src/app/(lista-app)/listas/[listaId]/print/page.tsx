53c1cd9a6323495f25ea1eb1ab6994d21987f056
yelsiño <yelsino321@gmai.com>
2023-10-30 18:43:45 -0500

    cambios

--- src/app/(lista-app)/listas/[listaId]/print/page.tsx
+++ src/app/(lista-app)/listas/[listaId]/print/page.tsx
@@ -3,6 +3,7 @@ import { useGetListaByIdQuery } from "@/redux/services/listaApi";
 import { convertListToPrint } from "@/util/print.util";
 import { getSession } from "next-auth/react";
 import { DATA } from "./parseo";
+import { PrintLista } from "@/interfaces/list.interface";
 
 
 interface IParams {
@@ -12,18 +13,91 @@ interface IParams {
 
 const PrintPage = ({ params }: { params: IParams }) => {
 
-    const getData = async () => {
-      const data = await DATA
-      return data
-    } 
+    // const getData = async () => {
+    //   const data:PrintLista = {
+    //     lista: {
+    //       id: "653d7d945eeb13168c4c67cc",
+    //       clienteId: "653ac480a66f1266efc16415",
+    //       numero: 5,
+    //       abonos: [],
+    //       montoTotal: 14,
+    //       pagado: false,
+    //       completado: true,
+    //       createdAt: "2023-10-28T21:31:00.149Z",
+    //       updatedAt: "2023-10-28T21:31:00.149Z",
+    //       items: [
+    //         {
+    //           id: "653d7d945eeb13168c4c67d1",
+    //           listaId: "653d7d945eeb13168c4c67cc",
+    //           nombre: "alberca",
+    //           precio: 0,
+    //           cantidad: 0,
+    //           montoItem: 2,
+    //           medida: "und",
+    //           texto: "2 soles de alberca",
+    //           calculado: true,
+    //           createdAt: "2023-10-28T21:31:00.149Z",
+    //           updatedAt: "2023-10-28T21:31:00.149Z",
+    //         },
+    //         {
+    //           id: "653d7d945eeb13168c4c67d2",
+    //           listaId: "653d7d945eeb13168c4c67cc",
+    //           nombre: "papa segunda",
+    //           precio: 0,
+    //           cantidad: 0,
+    //           montoItem: 4.7,
+    //           medida: "und",
+    //           texto: "4 soles 70 de papa segunda",
+    //           calculado: true,
+    //           createdAt: "2023-10-28T21:31:00.149Z",
+    //           updatedAt: "2023-10-28T21:31:00.149Z",
+    //         },
+    //       ],
+    //       cliente: {
+    //         id: "653ac480a66f1266efc16415",
+    //         usuarioId: "653ac480a66f1266efc16413",
+    //         nombres: "cliente tienda",
+    //         celular: "",
+    //         createdAt: "2050-01-01T00:00:00.000Z",
+    //         updatedAt: "2023-10-26T19:56:48.612Z",
+    //       },
+    //       errors: [],
+    //     },
+    //     tienda: {
+    //       id: "653ac480a66f1266efc16414",
+    //       usuarioId: "653ac480a66f1266efc16413",
+    //       nombreTienda: "tienda",
+    //       logo: "",
+    //       ubicacion: "",
+    //       texto1: "Bienvenido a nuestro negocio",
+    //       texto2: "de frutas y verduras",
+    //       numeroContacto: "",
+    //       email: "",
+    //       referencia: "",
+    //       urlTienda: "",
+    //       codigoQr: false,
+    //       codigoBarra: false,
+    //       createdAt: "2023-10-26T19:56:48.541Z",
+    //       updatedAt: "2023-10-26T19:56:48.541Z",
+    //     },
+    //   };
+    //   return data
+    // } 
 
     const handleClick = async () => {
         // Crear un elemento de canvas
 
-        const session: any = await getSession();
-        if (!session) return;
+        // const session: any = await getSession();
+        // if (!session) return;
 
-        const dataPrint = await convertListToPrint(await getData());
+        const dataPrint = {
+          fecha: "hoy",
+          montoTotal: 500,
+          nombreCliente: "JUAN VALDEZ",
+          nombreTienda: "DON PEPE",
+          texto1: "HOLA PE BATERIA",
+          texto2: "OTRA VEZ TU?",
+        }
 
         const fontLink: any = document.createElement('link');
         fontLink.href = 'https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600&display=swap';
