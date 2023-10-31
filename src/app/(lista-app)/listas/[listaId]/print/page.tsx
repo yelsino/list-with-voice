6bd60dfe43c5bee7ea7db7d38e0a56c902899895
yelsiño <yelsino321@gmai.com>
2023-10-30 19:04:34 -0500

    cambios5

--- src/app/(lista-app)/listas/[listaId]/print/page.tsx
+++ src/app/(lista-app)/listas/[listaId]/print/page.tsx
@@ -3,190 +3,135 @@ import { useGetListaByIdQuery } from "@/redux/services/listaApi";
 import { convertListToPrint } from "@/util/print.util";
 import { getSession } from "next-auth/react";
 import { DATA } from "./parseo";
-import { PrintLista } from "@/interfaces/list.interface";
+import { Lista, PrintLista } from "@/interfaces/list.interface";
+import axios from "axios";
+import { URLBASE } from "@/interfaces/constantes";
 
 
 interface IParams {
-    listaId: string;
-  }
-  
+  listaId: string;
+}
 
-const PrintPage = ({ params }: { params: IParams }) => {
+async function obtenerLista(listaId: string) {
+  const response = await axios.get(`${URLBASE.LOCAL}/listas/${listaId}`);
+  console.log("response:0", response.data);
 
-    // const getData = async () => {
-    //   const data:PrintLista = {
-    //     lista: {
-    //       id: "653d7d945eeb13168c4c67cc",
-    //       clienteId: "653ac480a66f1266efc16415",
-    //       numero: 5,
-    //       abonos: [],
-    //       montoTotal: 14,
-    //       pagado: false,
-    //       completado: true,
-    //       createdAt: "2023-10-28T21:31:00.149Z",
-    //       updatedAt: "2023-10-28T21:31:00.149Z",
-    //       items: [
-    //         {
-    //           id: "653d7d945eeb13168c4c67d1",
-    //           listaId: "653d7d945eeb13168c4c67cc",
-    //           nombre: "alberca",
-    //           precio: 0,
-    //           cantidad: 0,
-    //           montoItem: 2,
-    //           medida: "und",
-    //           texto: "2 soles de alberca",
-    //           calculado: true,
-    //           createdAt: "2023-10-28T21:31:00.149Z",
-    //           updatedAt: "2023-10-28T21:31:00.149Z",
-    //         },
-    //         {
-    //           id: "653d7d945eeb13168c4c67d2",
-    //           listaId: "653d7d945eeb13168c4c67cc",
-    //           nombre: "papa segunda",
-    //           precio: 0,
-    //           cantidad: 0,
-    //           montoItem: 4.7,
-    //           medida: "und",
-    //           texto: "4 soles 70 de papa segunda",
-    //           calculado: true,
-    //           createdAt: "2023-10-28T21:31:00.149Z",
-    //           updatedAt: "2023-10-28T21:31:00.149Z",
-    //         },
-    //       ],
-    //       cliente: {
-    //         id: "653ac480a66f1266efc16415",
-    //         usuarioId: "653ac480a66f1266efc16413",
-    //         nombres: "cliente tienda",
-    //         celular: "",
-    //         createdAt: "2050-01-01T00:00:00.000Z",
-    //         updatedAt: "2023-10-26T19:56:48.612Z",
-    //       },
-    //       errors: [],
-    //     },
-    //     tienda: {
-    //       id: "653ac480a66f1266efc16414",
-    //       usuarioId: "653ac480a66f1266efc16413",
-    //       nombreTienda: "tienda",
-    //       logo: "",
-    //       ubicacion: "",
-    //       texto1: "Bienvenido a nuestro negocio",
-    //       texto2: "de frutas y verduras",
-    //       numeroContacto: "",
-    //       email: "",
-    //       referencia: "",
-    //       urlTienda: "",
-    //       codigoQr: false,
-    //       codigoBarra: false,
-    //       createdAt: "2023-10-26T19:56:48.541Z",
-    //       updatedAt: "2023-10-26T19:56:48.541Z",
-    //     },
-    //   };
-    //   return data
-    // } 
-
-    const handleClick = async () => {
-        // Crear un elemento de canvas
-
-        // const session: any = await getSession();
-        // if (!session) return;
-
-        const dataPrint = {
-          fecha: "hoy",
-          montoTotal: 500,
-          nombreCliente: "JUAN VALDEZ",
-          nombreTienda: "DON PEPE",
-          texto1: "HOLA PE BATERIA",
-          texto2: "OTRA VEZ TU?",
-        }
-
-        const fontLink: any = document.createElement('link');
-        fontLink.href = 'https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600&display=swap';
-        fontLink.rel = 'stylesheet';
-        document.head.appendChild(fontLink);
-
-        fontLink.addEventListener('load', function () {
-            const width = 800;
-            const itemHeight = 50;
-            // const numItems = dataPrint.items.length;
-            const numItems = 5;
-            const height = 400 + numItems * itemHeight + 200; // Adjust the height based on the number of items
-
-            const canvas = document.createElement('canvas');
-            canvas.width = width;
-            canvas.height = height;
-            const ctx: any = canvas.getContext('2d');
-
-            // Dibujar un fondo
-            ctx.fillStyle = "white"; // Color de fondo
-            ctx.fillRect(0, 0, width, height);
-
-            // Dibujar el título
-            ctx.font = "bold 80px 'Dosis'";
-            ctx.fillStyle = "black";
-
-            const nombreTiendaX = (width - ctx.measureText("TIENDA YOLA").width) / 2
-            ctx.fillText("TIENDA YOLA", nombreTiendaX, 70);
-
-            // Dibujar Bienvenida
-            ctx.font = "37px 'Dosis'";
-            ctx.fillStyle = "#565656";
-            const texto1X = (width - ctx.measureText(dataPrint.texto1).width) / 2
-            const texto2X = (width - ctx.measureText(dataPrint.texto2).width) / 2
-            ctx.fillText(dataPrint.texto1, texto1X, 120);
-            ctx.fillText(dataPrint.texto2, texto2X, 160);
-
-            // // Datos del comercio
-            // ctx.font = "37px Dosis";
-            // ctx.fillText(`Fecha: ${dataPrint.fecha}`, 30, 230);
-            // ctx.fillText(`Cliente: ${dataPrint.nombreCliente}`, 30, 270);
-
-            // // Detalles de la compra
-            // ctx.font = "37px Dosis";
-            // let y = 370;
-
-            // ctx.fillText("Descripción", 30, y);
-            // ctx.fillText("Precio", 600, y);
-            // y += 70;
-
-            // dataPrint.items.forEach((item) => {
-
-            //     ctx.font = "Dosis 37px";
-            //     ctx.fillText(
-            //         item.texto,
-            //         30,
-            //         y
-            //     );
-
-            //     // Dibuja el monto normalmente
-            //     ctx.font = "37px Dosis";
-            //     ctx.fillText(item.monto, 600, y);
-            //     y += itemHeight;
-            // });
-
-            // // Dibujar el total
-            // ctx.font = "bold 37px Dosis";
-            // ctx.fillText(`Total: ${dataPrint.montoTotal}`, 30, height - 100);
-
-            // Convertir el canvas en una imagen
-            const image = new Image();
-            image.src = canvas.toDataURL();
-
-            // Crear un enlace de descarga
-            const link = document.createElement('a');
-            link.href = image.src;
-            link.download = 'imagen.png';
-
-            link.click();
-        })
+  return response
+}
 
 
-    };
+const PrintPage = async ({ params }: { params: IParams }) => {
 
-    return (
-        <div>
-            <button onClick={handleClick}>Generar Imagen y Descargar</button>
-        </div>
-    );
+  const session: any = await getSession();
+  if (!session) return;
+
+  const lista:any = await obtenerLista(params.listaId);
+
+
+  const handleClick = async () => {
+
+    const print = convertListToPrint({
+      tienda: session.user.tienda,
+      lista: lista
+    })
+
+    const dataPrint = {
+      fecha: "hoy",
+      montoTotal: 500,
+      nombreCliente: "JUAN VALDEZ",
+      nombreTienda: "DON PEPE",
+      texto1: "HOLA PE BATERIA",
+      texto2: "OTRA VEZ TU?",
+    }
+
+    const fontLink: any = document.createElement('link');
+    fontLink.href = 'https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600&display=swap';
+    fontLink.rel = 'stylesheet';
+    document.head.appendChild(fontLink);
+
+    fontLink.addEventListener('load', function () {
+      const width = 800;
+      const itemHeight = 50;
+      // const numItems = dataPrint.items.length;
+      const numItems = 5;
+      const height = 400 + numItems * itemHeight + 200; // Adjust the height based on the number of items
+
+      const canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      const ctx: any = canvas.getContext('2d');
+
+      // Dibujar un fondo
+      ctx.fillStyle = "white"; // Color de fondo
+      ctx.fillRect(0, 0, width, height);
+
+      // Dibujar el título
+      ctx.font = "bold 80px 'Dosis'";
+      ctx.fillStyle = "black";
+
+      const nombreTiendaX = (width - ctx.measureText("TIENDA YOLA").width) / 2
+      ctx.fillText("TIENDA YOLA", nombreTiendaX, 70);
+
+      // Dibujar Bienvenida
+      ctx.font = "37px 'Dosis'";
+      ctx.fillStyle = "#565656";
+      const texto1X = (width - ctx.measureText(dataPrint.texto1).width) / 2
+      const texto2X = (width - ctx.measureText(dataPrint.texto2).width) / 2
+      ctx.fillText(dataPrint.texto1, texto1X, 120);
+      ctx.fillText(dataPrint.texto2, texto2X, 160);
+
+      // // Datos del comercio
+      // ctx.font = "37px Dosis";
+      // ctx.fillText(`Fecha: ${dataPrint.fecha}`, 30, 230);
+      // ctx.fillText(`Cliente: ${dataPrint.nombreCliente}`, 30, 270);
+
+      // // Detalles de la compra
+      // ctx.font = "37px Dosis";
+      // let y = 370;
+
+      // ctx.fillText("Descripción", 30, y);
+      // ctx.fillText("Precio", 600, y);
+      // y += 70;
+
+      // dataPrint.items.forEach((item) => {
+
+      //     ctx.font = "Dosis 37px";
+      //     ctx.fillText(
+      //         item.texto,
+      //         30,
+      //         y
+      //     );
+
+      //     // Dibuja el monto normalmente
+      //     ctx.font = "37px Dosis";
+      //     ctx.fillText(item.monto, 600, y);
+      //     y += itemHeight;
+      // });
+
+      // // Dibujar el total
+      // ctx.font = "bold 37px Dosis";
+      // ctx.fillText(`Total: ${dataPrint.montoTotal}`, 30, height - 100);
+
+      // Convertir el canvas en una imagen
+      const image = new Image();
+      image.src = canvas.toDataURL();
+
+      // Crear un enlace de descarga
+      const link = document.createElement('a');
+      link.href = image.src;
+      link.download = 'imagen.png';
+
+      link.click();
+    })
+
+
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Generar Imagen y Descargar</button>
+    </div>
+  );
 }
 
 export default PrintPage;
--- src/util/print.util.ts
+++ src/util/print.util.ts
@@ -1,142 +1,30 @@
-import { URL_FONT_DOSIS } from "@/interfaces/constantes";
-import { Lista, PrintLista } from "@/interfaces/list.interface";
-import { Tienda } from "@/interfaces/user.interface";
-import { createCanvas, registerFont } from "canvas";
-import path from "path";
 
-export async function imageListGenerate(dataLit: PrintLista) {
-
-  const dataPrint = convertListToPrint(dataLit);
-  console.log("print2");
-  try {
-    const response = await fetch(URL_FONT_DOSIS);
-    const css = await response.text();
-
-    // Extrae la URL de la fuente WOFF2 desde el CSS
-    const match = css.match(/url\('([^']+)'\) format\('woff2'\)/);
-
-    console.log("match: ", match);
-
-    if (match && match[1]) {
-      const fontUrl = match[1];
-
-      // Registra la fuente en el contexto de Canvas
-      registerFont(fontUrl, { family: 'Dosis' });
-
-      console.log("Fuente Dosis registrada con éxito.");
-    }
-
-    const width = 800;
-
-    const itemHeight = 50;
-    const numItems = dataPrint.items.length;
-    const height = 400 + numItems * itemHeight + 200; // Adjust the height based on the number of items
-
-    const canvas = createCanvas(width, height);
-    const ctx = canvas.getContext("2d");
-    // Dibujar un fondo
-    ctx.fillStyle = "white"; // Color de fondo
-    ctx.fillRect(0, 0, width, height);
-
-    // Dibujar el título
-    ctx.font = "bold 80px Dosis";
-    ctx.fillStyle = "black";
-
-    const nombreTiendaX = (width - ctx.measureText(dataPrint.nombreTienda).width) / 2
-    ctx.fillText(dataPrint.nombreTienda, nombreTiendaX, 70);
-
-    // Dibujar Bienvenida
-    ctx.font = "37px Dosis";
-    ctx.fillStyle = "#565656";
-    const texto1X = (width - ctx.measureText(dataPrint.texto1).width) / 2
-    const texto2X = (width - ctx.measureText(dataPrint.texto2).width) / 2
-    ctx.fillText(dataPrint.texto1, texto1X, 120);
-    ctx.fillText(dataPrint.texto2, texto2X, 160);
-
-    // Datos del comercio
-    ctx.font = "37px Dosis";
-    ctx.fillText(`Fecha: ${dataPrint.fecha}`, 30, 230);
-    ctx.fillText(`Cliente: ${dataPrint.nombreCliente}`, 30, 270);
-
-    // Detalles de la compra
-    ctx.font = "37px Dosis";
-    let y = 370;
-
-    ctx.fillText("Descripción", 30, y);
-    ctx.fillText("Precio", 600, y);
-    y += 70;
-
-    dataPrint.items.forEach((item) => {
-
-      ctx.font = "Dosis 37px";
-      ctx.fillText(
-        item.texto,
-        30,
-        y
-      );
-
-      // Dibuja el monto normalmente
-      ctx.font = "37px Dosis";
-      ctx.fillText(item.monto, 600, y);
-      y += itemHeight;
-    });
-
-    // Dibujar el total
-    ctx.font = "bold 37px Dosis";
-    ctx.fillText(`Total: ${dataPrint.montoTotal}`, 30, height - 100);
-    console.log("print3");
-    const buffer = await new Promise<Buffer>((resolve, reject) => {
-        canvas.toBuffer((err, buf) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(buf);
-            }
-        });
-    });
-
-    return buffer
-
-    // const rutaImagen = path.join(process.cwd(), 'public', 'imagen.png');
-    // const fs = require('fs');
-    // const out = fs.createWriteStream(rutaImagen);
-    // const stream = canvas.createPNGStream();
-    // stream.pipe(out);
-    // out.on('finish', () => {
-    //   console.log('Imagen creada exitosamente.');
-    // });
-    // console.log("print4");
-    // console.info("FIN DE GENERAR IMAGEN");
-  } catch (error: any) {
-    console.error("ERROR AL GENERAR IMAGEN" + error.message);
-    return error;
-  }
-
-  // return { props: { data: "hola mundo" } }
-}
-
-
-export function convertListToPrint(dataLit: PrintLista) {
+export function convertListToPrint(dataLit:any) {
   const { lista, tienda } = dataLit;
 
-  return {
+  const datos =  {
     fecha: dateFormat(lista.createdAt),
     montoTotal: moneyFormatSimbol(lista.montoTotal),
     nombreCliente: lista?.cliente?.nombres ?? "",
     nombreTienda: tienda.nombreTienda.toUpperCase(),
     texto1: tienda.texto1,
     texto2: tienda.texto2,
-    items: lista.items.map((item: any) => {
+    // items: lista.items.map((item: any) => {
 
-      const texto = item.calculado ? `${item.nombre}` : `${item.cantidad} ${item.medida} ${item.nombre} ${item.precio} x und`;
-      const monto = moneyFormat(item.montoItem);
+    //   const texto = item.calculado ? `${item.nombre}` : `${item.cantidad} ${item.medida} ${item.nombre} ${item.precio} x und`;
+    //   const monto = moneyFormat(item.montoItem);
 
-      return {
-        texto: texto,
-        monto: monto
-      }
-    }),
+    //   return {
+    //     texto: texto,
+    //     monto: monto
+    //   }
+    // }),
   };
+
+
+  console.log("DATOS: ", datos);
+  
+  return datos 
 
 }
 
